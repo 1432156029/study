cba5de15cec56f35fd47a45871b9b60863ee8b8f
1432156029 <crystal19980601@sina.com>
2018-09-19 11:04:01 +0800

    Initial commit

---
+++ app/src/main/java/todaynews/itheima/com/recycledemo/MainActivity.java
@@ -0,0 +1,13 @@
+package todaynews.itheima.com.recycledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
